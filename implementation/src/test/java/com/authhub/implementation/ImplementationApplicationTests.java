4d237e216d9457e584ef0a31f57fb95ac578ea03
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-12-27 16:25:16 +0900

    feat : initialize project

---
+++ implementation/src/test/java/com/authhub/implementation/ImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authhub.implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
